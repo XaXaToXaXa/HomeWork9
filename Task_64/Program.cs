8d5aedb78547a111daead26e40fab11a66401581
XaXaToXaXa <XaXaToXaXa@gmail.com>
2023-02-01 19:54:48 +0300

    Edit first task

--- Task_64/Program.cs
+++ Task_64/Program.cs
@@ -4,14 +4,20 @@
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 Console.Write("Введите положительное целое число: ");
-int number = int.Parse(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
-Console.Write($"{number}, => ");
+if (number > 1)
+{
+Console.Write($"N = {number} => ");
 NumbersLine(number);
+Console.Write("1");
+Console.WriteLine();
 
-void NumbersLine(int n)
+void NumbersLine(int n) // Для красоты вывода выход происходиит на 2
 {
     Console.Write($"{n}, ");
-    if (n == 1) return;
+    if (n == 2) return;
     else NumbersLine(n - 1);
 }
+}
+else Console.WriteLine("Число дожно быть больше 1");
